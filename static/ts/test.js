6b058a470de45006eac577f5eab3bef349795c62
sx202 <2191365027@qq.com>
2019-08-22 06:19:44 +0800

    2019-08-22

--- static/ts/test.js
+++
@@ -1,5 +0,0 @@
-function greeter(person) {
-    return "hello ," + person;
-}
-var user = "sx202";
-document.body.innerHTML = greeter(user);
--- static/ts/test.ts
+++
@@ -1,7 +0,0 @@
-function greeter(person){
-    return "hello ," + person;
-}
-
-let user = "sx202";
-
-document.body.innerHTML = greeter(user);
